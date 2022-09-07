462070e3ef195b8fe1f642a0a57ca83116908985
Chad Johnston <cjohnston@megatome.com>
2022-09-06 20:16:45 -0600

    Start work on unit tests

--- src/app/components/contact-table/contact-table.component.spec.ts
+++ src/app/components/contact-table/contact-table.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { ContactTableComponent } from "./contact-table.component";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe("ContactTableComponent", () => {
   let component: ContactTableComponent;
@@ -8,7 +9,10 @@ describe("ContactTableComponent", () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ContactTableComponent ]
+      declarations: [ ContactTableComponent ],
+      imports: [
+        HttpClientTestingModule
+      ]
     })
       .compileComponents();
 
